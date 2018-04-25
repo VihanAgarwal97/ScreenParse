eb170dcca5465247e18b30fb3891b9fc157b2164
Vihan Agarwal <>
2018-04-24 23:29:57 -0500

    - dialogue and action are now parsed - minor fixes need to be made

--- code/scrapescript.py
+++ code/scrapescript.py
@@ -1,13 +1,15 @@
 '''
 This file will run through a script and
-convert it into an intermediate csv format
+convert it into an intermediate text format
 from which it will be converted into a
 character interaction table.
 '''
 
 #Import required modules
 from bs4 import BeautifulSoup
+from bs4 import Tag
 import csv
+import re
 
 with open('./data/deadpool.html') as file:
     soup = BeautifulSoup(file,'html.parser')
@@ -27,15 +29,55 @@ with open('./data/deadpool_alias.csv', 'rb') as aliases:
     for row in reader:
         movie_alias[row[0]] = set()
 
-i = 0
+
 for b in soup.select('b'):
+    #remove extra whitespace
     element = b.text.strip()
+    #print(element)
+    #remove words in parentheses to get rid of (CONTD.)
+    element = re.sub(r'\([^)]*\)', '', element)
+    element = element.strip()
+    #print(element)
+
+
     if "INT." in element or "EXT." in element:
-        scenes.append(element)
-    elif element in movie_alias:
-        characters.append(element)
-        print(b.next_sibling)
-        print("*****")
-    if i > 50:
-        break
-    i+=1
+        print("Scene: " + element)
+    elif element in movie_alias and element != "":
+        print("Character: " + element)
+    if isinstance(b.next_sibling, Tag):
+        continue
+
+    block = b.next_sibling.encode(encoding="UTF-8")
+    block = re.sub(r'\([^)]*\)', '', block, re.M)
+    lines = []
+    lines = block.split('\n')
+    dialogue = ""
+    action = ""
+    for line in lines:
+        if line.startswith("           "):
+            line = line.strip()
+            if line != "":
+                dialogue = dialogue + " " + line
+        else:
+            line = line.strip()
+            if line != "":
+                action = action + " " + line
+
+    dialogue = re.sub(r'[:]','',dialogue)
+    dialogue = re.sub(r'\s+',' ', dialogue)
+    dialogue = re.sub(r'\.+','.', dialogue)
+    dialogue = dialogue.strip()
+    action = re.sub(r'[:]','',action)
+    action = re.sub(r'\s+', ' ', action)
+    action = re.sub(r'\.+', '.', action)
+    action = action.strip()
+
+    if dialogue != "":
+        print("Dialogue: " + dialogue)
+    if action != "":
+        print("Action: " + action)
+    print("*****")
+
+# cow = "DEADPOOL (CONT'D)"
+# cow = re.sub(r'\([^)]*\)', '', cow)
+# print(cow)
