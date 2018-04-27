840575305a037370ed04fb9f1d83eb65afc3fab3
Vihan Agarwal <>
2018-04-26 20:12:42 -0500

    - screenplay is now converted to an intermediate format, but code needs restructuring

--- code/scrapescript.py
+++ code/scrapescript.py
@@ -8,8 +8,9 @@ character interaction table.
 #Import required modules
 from bs4 import BeautifulSoup
 from bs4 import Tag
-import csv
+import os
 import re
+import csv
 
 with open('./data/deadpool.html') as file:
     soup = BeautifulSoup(file,'html.parser')
@@ -19,9 +20,6 @@ with open('./data/deadpool.html') as file:
 #Value: Set of aliases of a character
 movie_alias = dict()
 
-#temp
-characters = []
-scenes = []
 
 #Read the csv alias file and store it in movie_alias
 with open('./data/deadpool_alias.csv', 'rb') as aliases:
@@ -29,55 +27,51 @@ with open('./data/deadpool_alias.csv', 'rb') as aliases:
     for row in reader:
         movie_alias[row[0]] = set()
 
+file_path = os.path.abspath('./out/updated_script.txt')
+with open(file_path,'w+') as updated_file:
+    for b in soup.select('b'):
+        #remove extra whitespace
+        element = b.text.strip()
+        #remove words in parentheses to get rid of (CONTD.)
+        element = re.sub(r'\([^)]*\)', '', element)
+        element = element.strip()
 
-for b in soup.select('b'):
-    #remove extra whitespace
-    element = b.text.strip()
-    #print(element)
-    #remove words in parentheses to get rid of (CONTD.)
-    element = re.sub(r'\([^)]*\)', '', element)
-    element = element.strip()
-    #print(element)
+        character = ""
+        if "INT." in element or "EXT." in element:
+            updated_file.write('----------------------------------------\n')
+            updated_file.write('# ' + element + '\n')
+        elif element in movie_alias and element != '':
+            updated_file.write(element+': ')
+            character = element
+        if isinstance(b.next_sibling, Tag):
+            continue
 
+        block = b.next_sibling.encode(encoding="UTF-8")
+        block = re.sub(r'\([^)]*\)', '', block, re.M)
+        lines = []
+        lines = block.split('\n')
+        dialogue = ""
+        action = ""
+        for line in lines:
+            if line.startswith("           "):
+                line = line.strip()
+                if line != "" and character != "":
+                    dialogue = dialogue + " " + line
+            else:
+                line = line.strip()
+                if line != "":
+                    action = action + " " + line
 
-    if "INT." in element or "EXT." in element:
-        print("Scene: " + element)
-    elif element in movie_alias and element != "":
-        print("Character: " + element)
-    if isinstance(b.next_sibling, Tag):
-        continue
+        dialogue = re.sub(r'[:]','',dialogue)
+        dialogue = re.sub(r'\.{3}',' ', dialogue)
+        dialogue = re.sub(r'\s+',' ', dialogue)
+        dialogue = dialogue.strip()
+        action = re.sub(r'[:]','',action)
+        action = re.sub(r'\.{3}', ' ', action)
+        action = re.sub(r'\s+', ' ', action)
+        action = action.strip()
 
-    block = b.next_sibling.encode(encoding="UTF-8")
-    block = re.sub(r'\([^)]*\)', '', block, re.M)
-    lines = []
-    lines = block.split('\n')
-    dialogue = ""
-    action = ""
-    for line in lines:
-        if line.startswith("           "):
-            line = line.strip()
-            if line != "":
-                dialogue = dialogue + " " + line
-        else:
-            line = line.strip()
-            if line != "":
-                action = action + " " + line
-
-    dialogue = re.sub(r'[:]','',dialogue)
-    dialogue = re.sub(r'\s+',' ', dialogue)
-    dialogue = re.sub(r'\.+','.', dialogue)
-    dialogue = dialogue.strip()
-    action = re.sub(r'[:]','',action)
-    action = re.sub(r'\s+', ' ', action)
-    action = re.sub(r'\.+', '.', action)
-    action = action.strip()
-
-    if dialogue != "":
-        print("Dialogue: " + dialogue)
-    if action != "":
-        print("Action: " + action)
-    print("*****")
-
-# cow = "DEADPOOL (CONT'D)"
-# cow = re.sub(r'\([^)]*\)', '', cow)
-# print(cow)
+        if dialogue != "":
+            updated_file.write(dialogue + '\n')
+        if action != "":
+            updated_file.write('###  [' + action + '] \n')
